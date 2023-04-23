c0947dde673dd9f372362eb206c8c02b348fed4b
QIXUAN WU <q.wu.22@abdn.ac.uk>
2023-04-23 20:41:31 +0000

    change database and add html

--- goods/models.py
+++ goods/models.py
@@ -10,6 +10,15 @@ class HomeDcor(models.Model):
     def __str__(self):
         return f'{self.rank}, {self.name}, {self.ratings}, {self.price}'
 
+class HomeDecorDetail(models.Model): 
+    name = models.TextField()
+    price = models.TextField()
+    category = models.TextField()
+    image = models.TextField()
+
+    def __str__(self):
+        return f'{self.name}, {self.price}, {self.category}, {self.image}'
+
 class HomeFurnishing(models.Model):
     rank = models.TextField(primary_key=True)
     name = models.TextField()
@@ -19,11 +28,11 @@ class HomeFurnishing(models.Model):
     def __str__(self):
         return f'{self.rank}, {self.name}, {self.ratings}, {self.price}'
 
-class HomeImprovement(models.Model):
-    rank = models.TextField(primary_key=True)
+class HomeFurnishingDetail(models.Model):
     name = models.TextField()
-    ratings = models.TextField()
     price = models.TextField()
+    category = models.TextField()
+    image = models.TextField()
 
     def __str__(self):
-        return f'{self.rank}, {self.name}, {self.ratings}, {self.price}'
+        return f'{self.name}, {self.price}, {self.category}, {self.image}'
