ffc87ebf160fd4c47e2b348b33c058168c6225a0
QIXUAN WU <q.wu.22@abdn.ac.uk>
2023-04-25 23:45:47 +0000

    Add new feature

--- goods/urls.py
+++ goods/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.display_index,name='display_index'),
 	path('home/', views.display_home,name='display_home'),
 	path('Dcor/', views.display_HomeDcor,name='display_HomeDcor'),
+	path('my_page/', views.datapage1, name='datapage1'),
 ]
--- goods/views.py
+++ goods/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from .models import HomeDcor,HomeFurnishing,HomeDecorDetail,HomeFurnishingDetail
 from django.shortcuts import render
 
@@ -11,3 +12,10 @@ def display_home(request):
 def display_HomeDcor(request):
     home_dcor_list = HomeDcor.objects.all()
     return render(request, 'Dcor.html', {'home_dcor_list': home_dcor_list})
+
+def datapage1(request):
+    homedcor = HomeDcor.objects.all()
+    paginator = Paginator(my_objects, 10) 
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'Dcor.html', {'page_obj': page_obj})
