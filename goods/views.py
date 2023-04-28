04dd8573fb0b429ff3817731b4426ebc2c918550
QIXUAN WU <q.wu.22@abdn.ac.uk>
2023-04-28 17:11:34 +0000

    change page

--- goods/views.py
+++ goods/views.py
@@ -1,6 +1,7 @@
 from django.core.paginator import Paginator
 from .models import HomeDcor,HomeFurnishing,HomeDecorDetail,HomeFurnishingDetail
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
 
 def display_index(request):
     return render(request, 'index.html')
@@ -9,13 +10,16 @@ def display_index(request):
 def display_home(request):
     return render(request, 'home.html')
 
-def display_HomeDcor(request):
+def display_HomeDcor(request,):
     homedcor = HomeDcor.objects.all()
-    paginator = Paginator(homedcor, 8) 
+    paginator = Paginator(homedcor, 8)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
     return render(request, 'Dcor.html', {'page_obj': page_obj})
 
+def display_HomeDcorDetail(request,name):
+    homedcordetail=get_object_or_404(HomeDcor, name=name)
+    return render(request, 'templates/Dcor_detail.html', {'homedcordetail':  homedcordetail})
 
 
 
