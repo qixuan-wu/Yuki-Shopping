d49d2a8fc196978289a573e3281eb72a594753a7
QIXUAN WU <q.wu.22@abdn.ac.uk>
2023-05-02 11:32:04 +0000

    change account

--- account/views.py
+++ account/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login
 from django.contrib import messages
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
-from django.contrib.auth import logout
+from django.contrib.auth import authenticate,login,logout
 from django.urls import reverse
 
 
@@ -21,7 +21,7 @@ def register(request):
             return render(request,'create.html')
         cuser = User.objects.create_user(username=username, password=password1, email=email)
         cuser.save()
-        return redirect('login')
+        return redirect('display_home')
     return render(request, 'create.html')
 
 
@@ -29,7 +29,7 @@ def login_view(request):
     if request.method == 'POST':
         form = AuthenticationForm(request, data=request.POST)
         if form.is_valid():
-            user = form.get_user('')
+            user = form.get_user()
             loin(request, user)
             messages.success(request,'successful login')
             return redirect('display_home')
@@ -37,10 +37,10 @@ def login_view(request):
             msg = 'Invalid login credentials'
             messages.error(request, msg)
     else:
-        form = AuthenticationForm(request)
+        form = AuthenticationForm()
     return render(request, 'login.html', {'form': form})
 
 
 def logout_view(request):
     logout(request)
-    return redirect(request.META.get('HTTP_REFERER',reverse('display_home')))
+    return redirect(reverse('display_home'))
--- goods/views.py
+++ goods/views.py
@@ -35,34 +35,3 @@ def display_HomeDcorDetail(request, detail_rank):
 
 
 
-# def add_to_cart(request):
-#     if request.method == 'POST':
-#         rank = request.POST['rank']
-#         name = request.POST['name']
-#         price = request.POST['price']
-#         quantity = request.POST.get('quantity', 1)
-#         cartlist, created = cart.objects.get_or_create(
-#             rank=rank,
-#             name=name,
-#             price=price,
-#             quantity=quantity
-#         )
-#         print(rank, name, price, cartlist)
-#         cartlist = cart.objects.all()
-#         return render(request, 'shoppingcar.html', {'cartlist': cartlist, 'new_product': {'rank': rank, 'name': name, 'price': price}})
-
-#     else:
-#         cartlist = cart.objects.all()
-#         return render(request, 'shoppingcar.html', {'cartlist': cartlist, 'new_product': {'rank': rank, 'name': name, 'price': price}})
-
-
-# def remove_from_cart(request):
-#     if request.method == 'POST':
-#         rank = request.POST['rank']
-#         name = request.POST['name']
-#         price = request.POST['price']
-#         cart.objects.filter(rank=rank, name=name, price=price).delete()
-#         cartlist = cart.objects.all()
-#         return render(request, 'shoppingcar.html', {'cartlist': cartlist})
-
-
